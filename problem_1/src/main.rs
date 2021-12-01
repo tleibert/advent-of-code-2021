57ec2c1d1e2ebf60a10d2c8db7303aefd70cfc80
Trevor Leibert <tleibert1999@gmail.com>
2021-12-01 01:38:34 -0500

    Solve part 1 of prob_1

--- problem_1/src/main.rs
+++ problem_1/src/main.rs
@@ -2,6 +2,32 @@
 //!
 //! Written by Trevor Leibert
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
     println!("Hello, world!");
+
+    let fh = File::open("inputs/input_1.txt").unwrap();
+    let reader = BufReader::new(fh);
+
+    let ans = count_depth_increases(reader);
+
+    println!("Number of depth increases: {}", ans);
+}
+
+fn count_depth_increases(reader: BufReader<File>) -> i32 {
+    let mut sum = -1; // don't count first line
+    let mut prev = 0;
+
+    for depth in reader.lines().map(|line| line.unwrap().parse().unwrap()) {
+        if depth > prev {
+            sum += 1;
+        }
+
+        prev = depth;
+    }
+
+    sum
 }
